53533cd61abbc1b3acb12c5bb65d5c8ee30a4b6d
hassansuhaib <ch.hassan.suhaib@gmail.com>
2020-03-20 09:25:48 +0500

    some additions

---
+++ brains.js
@@ -0,0 +1,8 @@
+const calculator = document.querySelector(‘.calculator’)
+const keys = calculator.querySelector(‘.calculator__keys’)
+
+keys.addEventListener(‘click’, e => {
+ if (e.target.matches(‘button’)) {
+   // Do something
+ }
+})
