393e0e879dd802e8d42769a1be44b270758c648f
hassansuhaib <ch.hassan.suhaib@gmail.com>
2020-03-20 10:25:53 +0500

    done

--- brains.js
+++ brains.js
@@ -1,8 +1,14 @@
-const calculator = document.querySelector(‘.calculator’)
-const keys = calculator.querySelector(‘.calculator__keys’)
-
-keys.addEventListener(‘click’, e => {
- if (e.target.matches(‘button’)) {
-   // Do something
- }
-})
+//function for displaying values in the display area
+function dis(val) {
+  document.getElementById("display").value += val;
+}
+//function for calculation
+function solve() {
+  let x = document.getElementById("display").value;
+  let y = eval(x);
+  document.getElementById("display").value = y;
+}
+//function for clearing the display
+function clr() {
+  document.getElementById("display").value = "";
+}
